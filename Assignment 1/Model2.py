12fca5ae1925e007e3b722bd2e69600e4b6fe2c8
FrederikFertin <s203679@student.dtu.dk>
2023-10-02 13:03:09 +0200

    Created csv of optimal bids

--- Assignment 1/Model2.py
+++ Assignment 1/Model2.py
@@ -16,95 +16,51 @@ from createXY import getPrices
 
 #%%Prepare price data
 cwd = os.getcwd()
-f_up = os.path.join(cwd,'Up-regulation price_2021.csv')
+
+filename = os.path.join(cwd, 'Prices.csv')
+prices = pd.read_csv(filename)
+spot = np.array(prices['Spot'])
+up = np.array(prices['Up'])
+down = np.array(prices['Down'])
+
+filename = os.path.join(cwd, 'wind power clean.csv')
+power = pd.read_csv(filename)
+power = np.array(power['Actual'])
 
 
-f_ahead = os.path.join(cwd,'Day-ahead price.xlsx')
-
-
-#dayahead - selecting the correct price arrea, replacing the commas
-#df_ahead = pd.read_csv(r"C:\Users\julia\OneDrive\Dokumente\DTU\machine L for E\Assignment 1\Day-ahead price.csv", sep = ";")
-df_ahead = pd.read_excel(f_ahead)
-df_ahead = df_ahead[df_ahead['PriceArea'] == 'DK2'].reset_index(drop = True)
-df_ahead = df_ahead.stack().str.replace(',', '.').unstack()
-
-#up and down regulation - creating columns and deleting the " in the data
-df_up2022 = pd.read_csv(r"C:\Users\julia\OneDrive\Dokumente\DTU\machine L for E\Assignment 1\Up-regulation price_2022.csv", sep = ",")
-df_down2022 = pd.read_csv(r"C:\Users\julia\OneDrive\Dokumente\DTU\machine L for E\Assignment 1\Down-regulation price_2022.csv", sep = ",")
-
-target_value = '"'
-df_up2022 = df_up2022.stack().str.replace(target_value,'').unstack()
-df_down2022 = df_down2022.stack().str.replace(target_value,'').unstack()
-
-# Rename Columns
-# Define a dictionary to map old column names to new column names
-column_mappingup = {col: col.replace('"', '') for col in df_up2022.columns}
-column_mappingdown = {col: col.replace('"', '') for col in df_down2022.columns}
-# Rename the columns using the mapping
-df_up2022.rename(columns=column_mappingup, inplace=True)
-df_down2022.rename(columns=column_mappingdown, inplace=True)
-
-#separating into 6 columns
-df_up2022[['Start time UTC','End time UTC','Start time UTC+02:00','End time UTC+02:00','Up-regulating price in the Balancing energy market']]= df_up2022['Start time UTC,End time UTC,Start time UTC+02:00,End time UTC+02:00,Up-regulating price in the Balancing energy market'].str.split(",", expand=True)
-df_down2022[['Start time UTC','End time UTC','Start time UTC+02:00','End time UTC+02:00','Down-regulation price in the Balancing energy market']]= df_down2022['Start time UTC,End time UTC,Start time UTC+02:00,End time UTC+02:00,Down-regulation price in the Balancing energy market'].str.split(",", expand=True)
-
-#print(df_up2022)
-#print(df_down2022)
-
-print(df_ahead)
-
-#%%Create parameters for the model
-
-power = [0.290184922, 0.258890469, 0.234708393, 0.223328592, 0.172119488, 0.160739687, 0.129445235, 0.095305832,
-0.100995733, 0.120910384, 0.112375533, 0.118065434, 0.110953058, 0.073968706, 0.055476529, 0.044096728,
-0.038406828, 0.034139403, 0.031294452, 0.031294452, 0.029871977, 0.027027027, 0.024182077, 0.024182077] #predicted power, used as acutal power for the next day
-price_ahead = df_ahead.loc[1:24,'SpotPriceEUR'].reset_index(drop = True) #price in the first hour UTC of 2022
-price_up = df_up2022.loc[2:25,'Up-regulating price in the Balancing energy market'].reset_index(drop = True)
-price_down = df_down2022.loc[2:25,'Down-regulation price in the Balancing energy market'].reset_index(drop = True)
-
-print(price_ahead)
-print(price_up)
-print(price_down)
 #%%Create optimization model
 model = gb.Model("Revenue")
 
 #create variables
-time = range(24)
-b = model.addVars(time, name = 'power bid')
+time = range(len(spot))
+b = model.addVars(time, vtype=GRB.CONTINUOUS, lb = 0, ub = 1, name = 'power_bid')
+over = model.addVars(time, vtype=GRB.CONTINUOUS, lb = 0, ub = 1, name = 'overproduction')
+under = model.addVars(time, vtype=GRB.CONTINUOUS, lb = 0, ub = 1, name = 'underproduction')
 a = model.addVars(time, vtype=GRB.BINARY, name='a')
-over = model.addVars(time, name = 'overproduction')
-under = model.addVars(time, name = 'underproduction')
-M = 0.01
 
-"""
-# Adding constraints for over and under production 
-#if power[t] > bid[t], then a = 1, otherwise a = 0
-model.addConstrs((power[t] >= b[t] - M * (1 - a) 
-                 for t in time), ({'name': 'M_constr1' + str(t)} for t in time))
-model.addConstrs((power[t] <= b[t] + M * a 
-                 for t in time), ({'name': 'M_constr2' + str(t)} for t in time))
-"""
-
-# Add indicator constraints
+# Add constraints
 model.addConstrs((over[t]  <= (power[t]-b[t]) * a[t]) for t in time)
 model.addConstrs((under[t] >= (b[t]-power[t]) * (1 - a[t])) for t in time)
 
-"""
-model.addConstr(((a==0) >> (over[t] == 0) for t in time),
-                     ({'name': "indicator_constr1" + str(t)} for t in time))
-model.addConstr(((a==0) >> (under[t] == (b[t] - power[t]) for t in time),
-                     ({'name': "indicator_constr2" + str(t)} for t in time)))
-model.addConstrs(((a == 1) >> ((over[t] == power[t]-b[t])  & (under[t] == 0))
-                for t in time), ({'name': "indicator_constr3" + str(t)} for t in time))
-"""
 
-
-objective = gb.quicksum(price_ahead[t]*b[t] + price_down[t]*over[t] - price_up[t]*under[t] for t in time)
+objective = gb.quicksum(spot[t]*b[t] + down[t]*over[t] - up[t]*under[t] for t in time)
 model.setObjective(objective, gb.GRB.MAXIMIZE)
 
-model.optimize() 
+model.optimize()
 
 optimal_variables = model.getVars()
 max_revenue = model.ObjVal
-print('optimal_variables:', optimal_variables)
+
 print('max_revenue:', max_revenue)
+
+#%% Save optimal bids
+bids = np.zeros(len(b))
+
+for i in range(len(b)):
+    bids[i] = b[i].x
+
+df_bid = pd.DataFrame()
+df_bid['Opt-Bid'] = bids
+
+filename = os.path.join(cwd,'optimal bids.csv')
+df_bid.to_csv(filename)
