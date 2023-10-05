bf21fbf8dbfa7fffab9b48ede7105e76878b30f1
FrederikFertin <s203679@student.dtu.dk>
2023-10-05 23:01:37 +0200

    missing packages

--- Assignment 1/model1.py
+++ Assignment 1/model1.py
@@ -2,6 +2,8 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import matplotlib.pyplot as plt
+import os
+import pandas as pd
 
 # Scikit-learn
 from sklearn.metrics import mean_squared_error
