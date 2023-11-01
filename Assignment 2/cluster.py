3e7dd17e0f8290ece0809cdb484f2786068f3292
FrederikFertin <s203679@student.dtu.dk>
2023-11-01 19:02:06 +0100

    created main (same shit) ;)

--- Assignment 2/cluster.py
+++ Assignment 2/cluster.py
@@ -116,7 +116,9 @@ class RLM:
         
         d_prices = p_test.copy()
         for i in range(self.n_levels):
-            if i < self.n_levels-1:
+            if i == 0:
+                d_prices.loc[(prices < self.price_cuts[i+1])] = self.price_levels[i]
+            elif i < self.n_levels-1:
                 d_prices.loc[(prices >= self.price_cuts[i]) & (prices < self.price_cuts[i+1])] = self.price_levels[i]
             else:
                 d_prices.loc[prices >= self.price_cuts[i]] = self.price_levels[i]
@@ -126,7 +128,7 @@ class RLM:
             
             s = np.where(self.socs == socs[-1])[0][0]
             
-            a = model.Pi[p,s]
+            a = self.Pi[p,s]
             price = prices.iloc[t]
             profits.append(profits[-1] + price * (-a))
             socs.append(socs[-1] + a)
@@ -149,97 +151,97 @@ def getPriceLevels(p_train,n_levels):
     
     return prices, price_levels, price_cuts
 
+if __name__ == "__main__":
+
+    trainsize = 0.75
+    cwd = os.getcwd()
+
+    filename = os.path.join(cwd, 'Prices.csv')
+
+    df = pd.read_csv(filename)
+    df["Hour"] = df.index.values % 24
+    prices = df.Spot
+    t_of_day = df.Hour
+
+    # p_train = prices[:int(len(prices)*(trainsize))]
+    # p_test = prices[int(len(prices)*(trainsize)):]
+
+    # %%
+    """
+    soc = np.linspace(0, 500, 6)
+    actions = np.array([-100,0,100])
+    price_levels = np.quantile(prices,[1/3,2/3])
     
-
-#%%
-trainsize = 0.75
-cwd = os.getcwd()
-
-filename = os.path.join(cwd,'Prices.csv')
-
-df = pd.read_csv(filename)
-df["Hour"] = df.index.values%24
-prices = df.Spot
-t_of_day = df.Hour
-
-
-#p_train = prices[:int(len(prices)*(trainsize))]
-#p_test = prices[int(len(prices)*(trainsize)):]
-
-#%%
-"""
-soc = np.linspace(0, 500, 6)
-actions = np.array([-100,0,100])
-price_levels = np.quantile(prices,[1/3,2/3])
-
-df["Discrete"] = df["Spot"]
-df["Discrete"].loc[df["Spot"] >= price_levels[1]] = 1
-df["Discrete"].loc[df["Spot"] < price_levels[1]] = 0
-df["Discrete"].loc[df["Spot"] < price_levels[0]] = -1
-
-
-plt.scatter(df.index,df.Spot,s=0.5,alpha=0.3)
-plt.title("Spot prices in 2021 and 2022")
-plt.axhline(price_levels[0],color='black',linestyle='--')
-plt.axhline(price_levels[1],color='black',linestyle='--')
-plt.show()
-
-high = df["Spot"].loc[df["Discrete"] == 1].mean()
-medium = df["Spot"].loc[df["Discrete"] == 0].mean()
-low = df["Spot"].loc[df["Discrete"] == -1].mean()
-
-"""
-
-#%% Creation of train and test data set
-train_days = 180
-test_days = 20
-
-df_train = df.iloc[:train_days*24,:]
-df_test = df.iloc[train_days*24:(train_days+test_days)*24,:]
-
-p_train = df_train['Spot']
-p_test = df_test['Spot']
-
-#%% Singular tests
-n_l = 3
-
-p_trains, p_levels, p_cuts = getPriceLevels(p_train, n_l)
-
-model = RLM(p_levels, p_cuts)
-model.calcPmatrix(p_trains, p_levels)
-
-values, iters = model.valueIter(gamma=0.99,maxIter=1000)
-
-profits, socs = model.test(p_test)
-
-plt.plot(profits)
-plt.show()
-
-#P = calcPmatrix(df)
-
-#%% Testing 
-del model
-gammas = np.linspace(0.99,0.9999,12-3)
-
-for ix, n_lev in enumerate(range(3,12)):
-    p_trains, p_levels, p_cuts = getPriceLevels(p_train, n_lev)
+    df["Discrete"] = df["Spot"]
+    df["Discrete"].loc[df["Spot"] >= price_levels[1]] = 1
+    df["Discrete"].loc[df["Spot"] < price_levels[1]] = 0
+    df["Discrete"].loc[df["Spot"] < price_levels[0]] = -1
     
+    
+    plt.scatter(df.index,df.Spot,s=0.5,alpha=0.3)
+    plt.title("Spot prices in 2021 and 2022")
+    plt.axhline(price_levels[0],color='black',linestyle='--')
+    plt.axhline(price_levels[1],color='black',linestyle='--')
+    plt.show()
+    
+    high = df["Spot"].loc[df["Discrete"] == 1].mean()
+    medium = df["Spot"].loc[df["Discrete"] == 0].mean()
+    low = df["Spot"].loc[df["Discrete"] == -1].mean()
+    
+    """
+
+    # %% Creation of train and test data set
+    train_days = 180
+    test_days = 50
+
+    df_train = df.iloc[:train_days * 24, :]
+    df_test = df.iloc[train_days * 24:(train_days + test_days) * 24, :]
+
+    p_train = df_train['Spot']
+    p_test = df_test['Spot']
+
+    # %% Singular tests
+    n_l = 3
+
+    p_trains, p_levels, p_cuts = getPriceLevels(p_train, n_l)
+
     model = RLM(p_levels, p_cuts)
     model.calcPmatrix(p_trains, p_levels)
 
-    values, iters = model.valueIter(gamma=gammas[ix],maxIter=1000)
+    values, iters = model.valueIter(gamma=0.99, maxIter=1000)
 
-    profitss, socss = model.test(p_test)
-    plt.title(str("Number of price categories/levels: " + str(n_lev)))
-    plt.scatter(np.arange(len(p_test)),p_test,color='black',alpha=0.1,s=0.1)
-    plt.plot(profitss)
-    plt.ylabel("Total profit")
-    plt.xlabel("Hour of trading in test market")
-    plt.show()
+    profits, socs = model.test(p_test)
     del model
 
-#plt.plot(socs)
-#plt.show()
+    plt.plot(profits)
+    plt.show()
+
+    # P = calcPmatrix(df)
+
+    # %% Testing
+
+    gammas = np.linspace(0.99, 0.9999, 12 - 3)
+
+    for ix, n_lev in enumerate(range(3, 12)):
+        p_trains, p_levels, p_cuts = getPriceLevels(p_train, n_lev)
+
+        model = RLM(p_levels, p_cuts)
+        model.calcPmatrix(p_trains, p_levels)
+
+        values, iters = model.valueIter(gamma=gammas[ix], maxIter=1000)
+
+        profitss, socss = model.test(p_test)
+        del model
+
+        plt.title(str("Number of price categories/levels: " + str(n_lev)))
+        plt.scatter(np.arange(len(p_test)), p_test, color='black', alpha=0.1, s=0.1)
+        plt.plot(profitss)
+        plt.ylabel("Total profit")
+        plt.xlabel("Hour of trading in test market")
+        plt.show()
+
+    # plt.plot(socs)
+    # plt.show()
 
 
 
