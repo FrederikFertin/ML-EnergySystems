33a9b960aab09c8f249f39aaed67c298fc2a2e35
FrederikFertin <s203679@student.dtu.dk>
2023-11-09 01:35:29 +0100

    merged

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -4,59 +4,23 @@ import os
 import matplotlib.pyplot as plt
 from rlm_discrete import RLM_discrete
 from rlm_continuous import RLM_continuous
-from functions import getPriceLevels, optimalBidding, cf_fit, continual_test
+from functions import getPriceLevels, optimalBidding, cf_fit, continual_test, create_train_test
 import seaborn as sns
-from distfit import distfit
 import plot
 
 
 #%% Preample
-trainsize = 0.75
 cwd = os.getcwd()
-
 filename = os.path.join(cwd, 'Prices.csv')
-
 df = pd.read_csv(filename)
 df["Hour"] = df.index.values % 24
 prices = df.Spot
 t_of_day = df.Hour
 
-# p_train = prices[:int(len(prices)*(trainsize))]
-# p_test = prices[int(len(prices)*(trainsize)):]
-
-# %%
-"""
-soc = np.linspace(0, 500, 6)
-actions = np.array([-100,0,100])
-price_levels = np.quantile(prices,[1/3,2/3])
-
-df["Discrete"] = df["Spot"]
-df["Discrete"].loc[df["Spot"] >= price_levels[1]] = 1
-df["Discrete"].loc[df["Spot"] < price_levels[1]] = 0
-df["Discrete"].loc[df["Spot"] < price_levels[0]] = -1
-
-
-plt.scatter(df.index,df.Spot,s=0.5,alpha=0.3)
-plt.title("Spot prices in 2021 and 2022")
-plt.axhline(price_levels[0],color='black',linestyle='--')
-plt.axhline(price_levels[1],color='black',linestyle='--')
-plt.show()
-
-high = df["Spot"].loc[df["Discrete"] == 1].mean()
-medium = df["Spot"].loc[df["Discrete"] == 0].mean()
-low = df["Spot"].loc[df["Discrete"] == -1].mean()
-
-"""
-
 # %% Creation of train and test data set
 train_days = 180
 test_days = 50
-
-df_train = df.iloc[:train_days * 24, :]
-df_test = df.iloc[train_days * 24:(train_days + test_days) * 24, :]
-
-p_train = df_train['Spot']
-p_test = df_test['Spot']
+p_train, p_test = create_train_test(df, train_days, len_of_train = 180, len_of_test = 50)
 
 # %% Singular tests
 n_l = 30
