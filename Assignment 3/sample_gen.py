f3c1c48461f9a0bf52b2c69ec91172d9f198dde1
Asmus <s200583@student.dtu.dk>
2023-11-15 17:00:37 +0100

    Added sample_gen.py containing functions to generate a dictionary of samples and to create a load profile from randomly picked samples

---
+++ Assignment 3/sample_gen.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import random
+
+def generate_samples(multipliers = [0.5,1,2,5]):
+    demand = pd.read_csv('./Demand.csv')
+    samples = pd.read_csv('./Samples.csv', header=None)
+    demand = np.vstack([list(map(float, row[0].split(';'))) for row in demand.values])
+    P_load_max = np.max(demand,axis=0)
+    samples = samples*P_load_max
+    samples_dict = {}
+    for i in multipliers:
+        samples_dict[i] = samples.copy()*i
+    return samples_dict
+
+def pick_samples(df, randseed = random.random()):
+    random.seed(randseed)
+    sample = np.empty([24,91])
+    for i in range(24):
+        sample[i,:] = df.iloc[random.randint(0,len(df))]
+    return sample
+
